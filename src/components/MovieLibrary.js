75fac09dee92476cf81d07c5fa7b76bd71f353b6
Jamila <J.cornick21@gmail.com>
2018-06-18 16:36:44 -0700

    created MovieLibrary component and return renders to page

---
+++ src/components/MovieLibrary.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import Movie from './Movie';
+import axios from 'axios';
+
+class MovieLibrary extends Component {
+
+
+  constructor() {
+    super();
+
+    this.state = {
+      movies: [],
+
+    };
+  }
+
+  componentDidMount() {
+     const MOVIE_URL = 'http://localhost:3000/movies'
+
+    axios.get(MOVIE_URL)
+    .then((response) => {
+      console.log(response);
+
+      this.setState({movies: response.data});
+    })
+    .catch((error) => {
+
+      // this.props.updateStatusCallback(error.message, 'error');
+
+    });
+  }
+
+  render() {
+
+    const movies =  this.state.movies.map((movie, index) => {
+
+      return(
+        <section>
+        < Movie key={index}
+        title={movie.title}
+        overview={movie.overview}
+        release_date={movie.release_date}
+        inventory={movie.inventory} />
+        </section>
+
+      )
+    });
+
+    return(
+      <section>
+      {movies}
+      </section>
+    )
+
+  }
+
+}
+
+export default MovieLibrary;
